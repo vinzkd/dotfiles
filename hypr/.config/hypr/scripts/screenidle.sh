b24c8d35dee8da40e3abbbcc4e1e9910f27619fe
Vinz <vinzk28@outlook.com>
2024-10-12 15:27:39 -0400

    doodoo water >:)

--- hypr/.config/hypr/scripts/screenidle.sh
+++ hypr/.config/hypr/scripts/screenidle.sh
@@ -2,15 +2,16 @@
 
 echo "Initializing hypridle.sh"
 
+# Check if hypridle is on.
 if pgrep -x "hypridle" > /dev/null
 then
     echo "hypridle is running"
     echo "Stopping hypridle"
-    notify-send -t 5000 -i  -e "Screen idle off"
+    notify-send -t 5000 -e "Screen idle off"
     killall hypridle
 else
     echo "hypridle is stopped"
     echo "Starting hypridle"
-    notify-send -t 5000 -i  -e "Screen idle on"
+    notify-send -t 5000 -e "Screen idle on"
     hypridle
 fi
